86050f1ebb4cfcec7d187d890edfe2dd62be76f2
甬哥kkkyg <90416692+kkkyg@users.noreply.github.com>
2022-05-12 12:02:30 +0800

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -124,6 +124,16 @@ update() {
         return 0
     fi
     arch=$(arch)
+    if [[ $arch == "x86_64" || $arch == "x64" || $arch == "amd64" ]]; then
+    arch="amd64"
+    elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
+    arch="arm64"
+    elif [[ $arch == "s390x" ]]; then
+    arch="s390x"
+    else
+    arch="amd64"
+    echo -e "${red}检测架构失败，使用默认架构: ${arch}${plain}"
+    fi
     systemctl stop x-ui
     cd /usr/local/
     if  [ $# == 0 ] ;then
